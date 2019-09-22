58e283d123ac6eda50b3938eba392bb0dd3ddaf0
Jalysa Reason <jalysareason@gmail.com>
2019-09-22 16:04:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,37 +2,24 @@
 def nested hash 
     it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
       (hopper)=({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
-
-    end
   end
 
-  describe "#alan_kay_is_known_for" do
-    it "operates on the programmer_hash and returns the value of what Alan Kay is known for" do 
-      expect(alan_kay_is_known_for).to eq("Object Orientation")
-    end 
+  def alan_kay_is_known_for
+      (alan_kay_is_known_for)=("Object Orientation")
   end
 
-  describe "#dennis_ritchies_language" do 
-    it "operates on the programmer_hash and returns the value of Dennis Ritchie's language as a string" do 
-      expect(dennis_ritchies_language).to eq("C")
-    end
+  def dennis_ritchies_language 
+      (dennis_ritchies_language)=("C")
   end
 
-  describe "#adding_matz" do 
-    it "operates on the programmer_hash and adds a key/value pair to the top level of the hash, returning the newly-added-to hash" do 
-      expect(adding_matz.keys).to include(:yukihiro_matsumoto)
-    end
+  def adding_matz
+      (adding_matz.keys).to include(:yukihiro_matsumoto)
   end
 
-  describe "#changing_alan" do 
-    it "operates on the programmer_hash and changes what Alan Kay is known for, returning the newly-changed hash" do 
-      expect(changing_alan[:alan_kay][:known_for]).to eq("GUI")
-    end
+  describe changing_alan 
+      (changing_alan[:alan_kay][:known_for])=("GUI")
   end
 
-  describe "#adding_to_dennis" do 
-    it "operates on the programmer_hash and adds 'Assembly' to Dennis Ritchie's languages, returning the newly-added-to-hash" do 
-      expect(adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
-    end
-  end
+  def adding_to_dennis 
+      adding_to_dennis[:dennis_ritchie][:languages][1]).to include("Assembly")
 end
